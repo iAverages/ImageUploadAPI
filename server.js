1940137e9f0d99cf7b5e0ed133cad2a696f0206a
iAvearges <31937542+iAvearges@users.noreply.github.com>
2021-03-17 12:58:31 +0000

    Added ratelimiting

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const fileupload = require("express-fileupload")
 const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require('morgan');
+const rateLimit = require("express-rate-limit");
+const mongoStore = require('rate-limit-mongo');
 const log = require("./helpers/logger");
 const app = express();
 require("dotenv").config()
@@ -10,6 +12,16 @@ require("dotenv").config()
 const port = process.env.PORT || 3000;
 const main = require("./routes/main");
 const api = require("./routes/api");
+const apiLimiter = rateLimit({
+    windowMs: 1000 * 60 * 10,
+    max: 20,
+    message: "Stop requesting my shit.",
+    store: new mongoStore({
+        uri: process.env.MONGO_URI,
+        user: process.env.MONGO_USER,
+        password: process.env.MONGO_PASSWORD
+    })
+})
 const middleware = [
     fileupload({
         preserveExtension: true,
@@ -24,8 +36,9 @@ const middleware = [
     morgan("combined"),
 ]
 
+app.set("trust proxy", "loopback");
 app.use(middleware);
 app.use("/", main);
-app.use("/api", api);
+app.use("/api", apiLimiter, api);
 
 app.listen(port, () => log.info(`Server started, listening on port ${port}`));

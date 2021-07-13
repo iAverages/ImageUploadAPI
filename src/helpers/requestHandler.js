d5764fec044a1e5eb400858eb7f9201efe3dd7e4
iAvearges <31937542+iAverages@users.noreply.github.com>
2021-07-13 12:34:49 +0100

    Fixed code smell from sonarcloud

--- src/helpers/requestHandler.js
+++ src/helpers/requestHandler.js
@@ -2,24 +2,24 @@ const format = (res, status = 200, message, data) => {
     // Construct the return json
     const json = { success: status < 400 };
     if (message) json.message = message;
-    if (data) json.data = data;
+    if (data && data.length > 0) json.data = data;
 
     res.status(status).json(json);
 };
 
-const success = (res, message = "", data) => {
+const success = (res, message = "", data = []) => {
     format(res, 200, message, data);
 };
 
-const error = (res, message = "", data) => {
+const error = (res, message = "", data = []) => {
     format(res, 500, message, data);
 };
 
-const badRequest = (res, message = "", data) => {
+const badRequest = (res, message = "", data = []) => {
     format(res, 400, message, data);
 };
 
-const notFound = (res, message = "404 Not Found", data) => {
+const notFound = (res, message = "404 Not Found", data = []) => {
     format(res, 404, message, data);
 };
 
